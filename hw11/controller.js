3bd5fddecf23dbbbb2f152d36cbbd9c7a7aeb076
Alexander Kravchenko <alex.krava@live.com>
2016-12-27 14:35:28 +0200

    fix hw11

--- hw11/controller.js
+++ hw11/controller.js
@@ -23,19 +23,41 @@ var Controller = {
     photosRoute: function () {
         return Model.getPhotos(0).then(function (photos) {
             let photosArray = [];
+            let countPhoto = 200;
 
-            if (photos[0] > 200) {
-                let count = Math.ceil(photos[0] / 200);
-                let offset;
+            if (photos[0] > countPhoto) {
+                let count = Math.ceil(photos[0] / countPhoto);
+                let offset = 0;
 
-                for (let i = 0; i < count; i++) {
-                    offset = 200 * i;
+                getPhoto(offset, count);
 
-                    Model.getPhotos(offset).then(function (photos) {
-                        photos.splice(0, 1);
-                        photosArray.push(photos[0]);
-                        results.innerHTML = View.render('photos', {list: photosArray});
-                    });
+                function getPhoto(offset, count){
+                    new Promise(function (resolve) {
+                        Model.getPhotos(offset).then(function (photos) {
+                            photos.splice(0, 1);
+                            for(photo of photos){
+                                photosArray.push(photo);
+                            }
+
+                            if(photosArray){
+                                resolve(photosArray);
+                            }
+
+                        });
+                    })
+                        .then(function (photosArray) {
+                            if(count){
+                                getPhoto((countPhoto + offset), --count);
+                                return;
+                            }
+
+                            return new Promise(function (resolve) {
+                                resolve(photosArray);
+                            });
+                        })
+                        .then(function (photosArray) {
+                                results.innerHTML = View.render('photos', {list: photosArray});
+                        });
                 }
             }
             else {
